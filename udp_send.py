28bfe64d4d8c927019f8daa5c61f17e2af6957bc
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-03-04 09:42:10 +0900

    Update udp_send.py

--- udp_send.py
+++ udp_send.py
@@ -3,10 +3,28 @@
 #https://ameblo.jp/oyasai10/entry-10590541669.html
 
 from socket import *
+import netifaces
+
+# Get IP address
+for iface_name in netifaces.interfaces():
+	iface_data = netifaces.ifaddresses(iface_name)
+	ipList=iface_data.get(netifaces.AF_INET)
+	#print("ip={}".format(ipList))
+	ipDict = ipList[0]
+	addr = ipDict["addr"]
+	print("addr={}".format(addr))
+	if (addr != "127.0.0.1"):
+		myIp = addr
+
+print("myIp={}".format(myIp))
+myIpList = myIp.split('.')
+print("myIpList={}".format(myIpList))
 
 HOST = ''
 PORT = 9876
-ADDRESS = "192.168.10.255" # for Broadcast
+#ADDRESS = "192.168.10.255" # for Broadcast
+ADDRESS = "{}.{}.{}.255".format(myIpList[0], myIpList[1], myIpList[2])
+print("ADDRESS={}".format(ADDRESS))
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
